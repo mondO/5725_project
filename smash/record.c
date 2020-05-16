140c09a6961abe21f8a367b35a4c699992dbf13f
Ray Yang <rly24@cornell.edu>
2020-05-16 19:39:42 -0400

    include code

---
+++ smash/record.c
@@ -0,0 +1,320 @@
+// CC-by-www.Electrosmash.com open-source project
+// this code reads all the hardware and print it on the screen
+ 
+#include <bcm2835.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+
+// Define Input Pins
+#define PUSH1 			RPI_GPIO_P1_08  	//GPIO14
+#define PUSH2 			RPI_V2_GPIO_P1_38  	//GPIO20 
+#define TOGGLE_SWITCH 	RPI_V2_GPIO_P1_32 	//GPIO12
+#define FOOT_SWITCH 	RPI_GPIO_P1_10 		//GPIO15
+#define LED   			RPI_V2_GPIO_P1_36 	//GPIO16
+#define BILLION  1000000000.0
+
+
+uint32_t read_timer=0;
+uint32_t input_signal=0;
+ 
+uint8_t FOOT_SWITCH_val;
+uint8_t TOGGLE_SWITCH_val;
+uint8_t PUSH1_val;
+uint8_t PUSH2_val;
+
+int isBigEndian() {
+    int test = 1;
+    char *p = (char*)&test;
+
+    return p[0] == 0;
+}
+void reverseEndianness(const long long int size, void* value){
+    int i;
+    char result[32];
+    for( i=0; i<size; i+=1 ){
+        result[i] = ((char*)value)[size-i-1];
+    }
+    for( i=0; i<size; i+=1 ){
+        ((char*)value)[i] = result[i];
+    }
+}
+void toBigEndian(const long long int size, void* value){
+    char needsFix = !( (1 && isBigEndian()) || (0 && !isBigEndian()) );
+    if( needsFix ){
+        reverseEndianness(size,value);
+    }
+}
+void toLittleEndian(const long long int size, void* value){
+    char needsFix = !( (0 && isBigEndian()) || (1 && !isBigEndian()) );
+    if( needsFix ){
+        reverseEndianness(size,value);
+    }
+}
+
+typedef struct WaveHeader {
+    // Riff Wave Header
+    char chunkId[4];
+    int  chunkSize;
+    char format[4];
+
+    // Format Subchunk
+    char subChunk1Id[4];
+    int  subChunk1Size;
+    short int audioFormat;
+    short int numChannels;
+    int sampleRate;
+    int byteRate;
+    short int blockAlign;
+    short int bitsPerSample;
+    //short int extraParamSize;
+
+    // Data Subchunk
+    char subChunk2Id[4];
+    int  subChunk2Size;
+
+} WaveHeader;
+
+WaveHeader makeWaveHeader(int const sampleRate,
+        short int const numChannels,
+        short int const bitsPerSample ){
+    WaveHeader myHeader;
+
+    // RIFF WAVE Header
+    myHeader.chunkId[0] = 'R';
+    myHeader.chunkId[1] = 'I';
+    myHeader.chunkId[2] = 'F';
+    myHeader.chunkId[3] = 'F';
+    myHeader.format[0] = 'W';
+    myHeader.format[1] = 'A';
+    myHeader.format[2] = 'V';
+    myHeader.format[3] = 'E';
+
+    // Format subchunk
+    myHeader.subChunk1Id[0] = 'f';
+    myHeader.subChunk1Id[1] = 'm';
+    myHeader.subChunk1Id[2] = 't';
+    myHeader.subChunk1Id[3] = ' ';
+    myHeader.audioFormat = 1; // FOR PCM
+    myHeader.numChannels = numChannels; // 1 for MONO, 2 for stereo
+    myHeader.sampleRate = sampleRate; // ie 44100 hertz, cd quality audio
+    myHeader.bitsPerSample = bitsPerSample; // 
+    myHeader.byteRate = myHeader.sampleRate * myHeader.numChannels * myHeader.bitsPerSample / 8;
+    myHeader.blockAlign = myHeader.numChannels * myHeader.bitsPerSample/8;
+
+    // Data subchunk
+    myHeader.subChunk2Id[0] = 'd';
+    myHeader.subChunk2Id[1] = 'a';
+    myHeader.subChunk2Id[2] = 't';
+    myHeader.subChunk2Id[3] = 'a';
+
+    // All sizes for later:
+    // chuckSize = 4 + (8 + subChunk1Size) + (8 + subChubk2Size)
+    // subChunk1Size is constanst, i'm using 16 and staying with PCM
+    // subChunk2Size = nSamples * nChannels * bitsPerSample/8
+    // Whenever a sample is added:
+    //    chunkSize += (nChannels * bitsPerSample/8)
+    //    subChunk2Size += (nChannels * bitsPerSample/8)
+    myHeader.chunkSize = 4+8+16+8+0;
+    myHeader.subChunk1Size = 16;
+    myHeader.subChunk2Size = 0;
+    
+    return myHeader;
+}
+
+typedef struct Wave {
+    WaveHeader header;
+    char* data;
+    long long int index;
+    long long int size;
+    long long int nSamples;
+} Wave;
+
+Wave makeWave(int const sampleRate, short int const numChannels, short int const bitsPerSample){
+    Wave myWave;
+    myWave.header = makeWaveHeader(sampleRate,numChannels,bitsPerSample);
+    return myWave;
+}
+
+void waveSetDuration( Wave* wave, const float seconds ){
+    long long int totalBytes = (long long int)(wave->header.byteRate*seconds);
+    wave->data = (char*)malloc(totalBytes);
+    wave->index = 0;
+    wave->size = totalBytes;
+    wave->nSamples = (long long int) wave->header.numChannels * wave->header.sampleRate * seconds;
+    wave->header.chunkSize = 4+8+16+8+totalBytes;
+    wave->header.subChunk2Size = totalBytes;
+}
+
+void waveDestroy( Wave* wave ){
+    free( wave->data );
+}
+
+void waveAddSample( Wave* wave, const float* samples ){
+    int i;
+    short int sample8bit;
+    int sample16bit;
+    long int sample32bit;
+    char* sample;
+    if( wave->header.bitsPerSample == 8 ){
+        for( i=0; i<wave->header.numChannels; i+= 1){
+            sample8bit = (short int) (127+127.0*samples[i]);
+            toLittleEndian(1, (void*) &sample8bit);
+            sample = (char*)&sample8bit;
+            (wave->data)[ wave->index ] = sample[0];
+            wave->index += 1;
+        }
+    }
+    if( wave->header.bitsPerSample == 16 ){
+        for( i=0; i<wave->header.numChannels; i+= 1){
+            sample16bit = (int) (32767*samples[i]);
+            //sample = (char*)&litEndianInt( sample16bit );
+            toLittleEndian(2, (void*) &sample16bit);
+            sample = (char*)&sample16bit;
+            wave->data[ wave->index + 0 ] = sample[0];
+            wave->data[ wave->index + 1 ] = sample[1];
+            wave->index += 2;
+        }
+    }
+    if( wave->header.bitsPerSample == 32 ){
+        for( i=0; i<wave->header.numChannels; i+= 1){
+            sample32bit = (long int) ((pow(2,32-1)-1)*samples[i]);
+            //sample = (char*)&litEndianLong( sample32bit );
+            toLittleEndian(4, (void*) &sample32bit);
+            sample = (char*)&sample32bit;
+            wave->data[ wave->index + 0 ] = sample[0];
+            wave->data[ wave->index + 1 ] = sample[1];
+            wave->data[ wave->index + 2 ] = sample[2];
+            wave->data[ wave->index + 3 ] = sample[3];
+            wave->index += 4;
+        }
+    }
+}
+
+void waveToFile( Wave* wave, const char* filename ){
+
+    // First make sure all numbers are little endian
+    toLittleEndian(sizeof(int), (void*)&(wave->header.chunkSize));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.subChunk1Size));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.audioFormat));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.numChannels));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.sampleRate));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.byteRate));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.blockAlign));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.bitsPerSample));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.subChunk2Size));
+
+    // Open the file, write header, write data
+    FILE *file;
+    file = fopen(filename, "wb");
+    fwrite( &(wave->header), sizeof(WaveHeader), 1, file );
+    fwrite( (void*)(wave->data), sizeof(char), wave->size, file );
+    fclose( file );
+
+    // Convert back to system endian-ness
+    toLittleEndian(sizeof(int), (void*)&(wave->header.chunkSize));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.subChunk1Size));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.audioFormat));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.numChannels));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.sampleRate));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.byteRate));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.blockAlign));
+    toLittleEndian(sizeof(short int), (void*)&(wave->header.bitsPerSample));
+    toLittleEndian(sizeof(int), (void*)&(wave->header.subChunk2Size));
+}
+
+void write_samples_to_file(int* samples, int nSamples, float sampleRate, float duration) {
+    // convert all samples from [0,1024] int to (-1.0, 1.0) floats
+    float sample_floats[nSamples];
+    int i;
+    for(i = 0; i < nSamples; i++) {
+        float sample_asfloat = ((float)(samples[i] - 512))/512.0;
+        sample_floats[i] = sample_asfloat;
+        /*printf("converted sample of %d to %f\n", samples[i], sample_floats[i]);*/
+    }
+    // Create a mono (1), 32-bit sound and set the duration
+    Wave mySound = makeWave((int)sampleRate,1,32);
+    waveSetDuration( &mySound, duration );
+
+    float frameData[1];
+    for(i=0; i<nSamples; i+=1 ){
+        frameData[0] = sample_floats[i];
+        waveAddSample( &mySound, frameData );
+    }
+    // Write it to a file and clean up when done
+    waveToFile( &mySound, "mono-32bit.wav");
+    waveDestroy( &mySound );
+}
+
+
+int main(int argc, char **argv)
+{
+    // Start the BCM2835 Library to access GPIO.
+    if (!bcm2835_init())
+    {
+      printf("bcm2835_init failed. Are you running as root??\n");
+      return 1;
+    }
+	// Start the SPI BUS.
+	if (!bcm2835_spi_begin())
+    {
+      printf("bcm2835_spi_begin failed. Are you running as root??\n");
+      return 1;
+    }
+ 
+	//define SPI bus configuration
+    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64); 	  // 4MHz clock with _64 
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
+ 
+    uint8_t start = 0x01;
+    uint8_t end = 0x00;
+    uint8_t chan = 0x00;
+    char buf[] = {start, (0x08|chan)<<4,end};
+    char readBuf[3];
+
+    int i = 0;	
+    struct timespec start_time, end_time;
+    double time_used;
+    double samp_period_us = 0.0182592 * 1000;
+
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    int num_samples = 900000;
+    int samples[num_samples];
+
+    while(i < num_samples) //Main Loop
+	{
+        //read 8 bits ADC
+        bcm2835_spi_transfernb(buf,readBuf,3);
+        input_signal =  ((int)readBuf[1] & 0x03) << 8 | (int) readBuf[2];
+        samples[i] = input_signal;
+
+        /*printf("input: %d\n", input_signal);*/
+
+        //generate output PWM signal 6 bits
+        /*bcm2835_pwm_set_data(1,input_signal & 0x1F);*/
+        /*bcm2835_pwm_set_data(0,input_signal >> 5);*/
+        i++;
+        /*usleep(samp_period_us);*/
+    }
+    
+
+    clock_gettime(CLOCK_REALTIME, &end_time);
+	time_used = (end_time.tv_sec - start_time.tv_sec) +
+				(end_time.tv_nsec - start_time.tv_nsec) / BILLION;
+    /*int rate = ((double) num_samples) ;*/
+    double rate = ((double) num_samples) / time_used;
+    printf("%d samples collected in %f seconds: rate of %f", num_samples, time_used, rate);
+    
+    write_samples_to_file(samples, num_samples, rate, time_used); 
+ 
+	//close all and exit
+	bcm2835_spi_end();
+    bcm2835_close();
+    return 0;
+}
+
